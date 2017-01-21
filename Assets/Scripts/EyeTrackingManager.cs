22e8dd1ad0ca461779bd9f408689ca38e3a696f2
Kim Ordén <kim.orden@maetproducts.net>
2017-01-21 02:01:46 +0200

    Support for eyetracking
    it works

---
+++ Assets/Scripts/EyeTrackingManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Tobii.EyeTracking;
+
+public class EyeTrackingManager : MonoBehaviour {
+
+	public Vector2 _gazespot;
+	public GazePoint _gazepoint;
+	public GameObject spotchecker;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		_gazepoint = EyeTracking.GetGazePoint ();
+		_gazespot = Camera.main.ScreenToWorldPoint (_gazepoint.Screen);
+
+		if (_gazepoint.IsValid && _gazepoint.IsWithinScreenBounds)
+		spotchecker.transform.position = _gazespot;
+	}
+}
