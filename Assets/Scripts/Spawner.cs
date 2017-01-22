ec3d4f625feffa2bf1aa508c999b0ecbf9e2bf5b
Kim Ordén <kim.orden@maetproducts.net>
2017-01-22 14:06:26 +0200

    Spawner with eyetracking for upgrades

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+	public GameObject[] spawns;
+	public float timer,cooldown;
+	public Transform[] spawnpoints;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		timer -= Time.deltaTime;
+		if (timer < 0) 
+		{
+			spawn (spawns[Random.Range(0,spawns.Length)],spawnpoints[Random.Range(0,spawnpoints.Length)]);
+			timer = cooldown;
+		}
+	}
+	void spawn(GameObject upgrade,Transform spawnlocation)
+	{
+		Instantiate ((GameObject)upgrade, spawnlocation.position,Quaternion.identity);
+	}
+}
